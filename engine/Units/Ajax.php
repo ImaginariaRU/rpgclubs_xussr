31d7da513a947a692d6ebf346296fd2507b9e894
Karel <karel.wintersky@gmail.com>
2018-02-05 05:15:59 +0300

    0.1.15 "Patrick"
    + разные шаблоны вывода информации
    + get_info_poi() new

--- engine/Units/Ajax.php
+++ engine/Units/Ajax.php
@@ -10,16 +10,32 @@
 
 namespace RPGCAtlas\Units;
 
+use RPGCAtlas\Classes\DBStatic;
+use RPGCAtlas\Classes\Template;
+
 class Ajax
 {
     public function get_info_poi($id)
     {
+        $dbi = DBStatic::getInstance();
+        $table = $dbi::$_table_prefix . 'clubs';
+        $query = "SELECT * FROM {$table} WHERE `id` = :id ORDER BY `id` DESC LIMIT 1";
+
+        $sth = $dbi->getConnection()->prepare($query);
+        $sth->execute([
+            'id'    =>  $id
+        ]);
+        $dataset = $sth->fetch();
+        $dataset['title'] = htmlspecialchars($dataset['title'], ENT_QUOTES | ENT_HTML5);
+
+        $template_name = $dataset['infobox_layout'] == 'VKBanner' ? 'img_horizontal.html' : 'img_vertical.html';
+
+        $template = new Template($template_name, '$/templates/info');
+
+        $template->set('dataset', $dataset);
 
 
-
-
-
-        return "AJAX return poi info {$id}";
+        return $template->render();
     }
 
     public function get_city_by_coords()
